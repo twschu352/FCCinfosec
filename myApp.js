ea597c59e9503af7467a6b204fdfe81d634eeda7
nouk-online <nour.issa12@gmail.com>
2024-11-11 12:30:25 -0500

    frameguard6

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'DENY'}));
+app.use(helmet.frameguard({action: 'deny'}));
 
 
 

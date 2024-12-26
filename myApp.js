3fe4dd37854b1cab848d6dec22b1911e5bc06d40
Jesus Guillermo  Granadillo Garcia <jesus140489@gmail.com>
2024-12-26 22:55:25 +0000

    22

--- myApp.js
+++ myApp.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+// app.disable('x-powered-by');
 
-const helmet = require('helmet');
+var helmet = require('helmet');
+// app.use(helmet.contentSecurityPolicy({
+//     directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'"], styleSrc:["'self'"] }
+//   }));
+
 app.use(helmet.hidePoweredBy());
 
+
+
+
+
+
 module.exports = app;
-const api = require('./server.js');
+var api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });

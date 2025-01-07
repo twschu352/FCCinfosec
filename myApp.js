36acc34fea090d7dc747db667906cd7d5cde1ef9
Daniel Leiva <ignacioleiva79@gmail.com>
2025-01-07 19:13:54 +0000

    ej2.1

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(helmet.hidePoweredBy());
+
 
 
 
@@ -59,3 +59,5 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
+app.use(helmet.hidePoweredBy());

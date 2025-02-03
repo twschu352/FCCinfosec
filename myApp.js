857c9e4676d74a4256dc798eb49597063639c9f2
ugmsn <eric.odey@gmail.com>
2025-02-03 20:17:23 +0100

    Required helmet.js

--- myApp.js
+++ myApp.js
@@ -1,59 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+// Your code starts here
+const helmet = require("helmet");
 
+// Use helmet middleware
+app.use(helmet());
 
+// Your code ends here
 
+// Existing code provided by the course
+// Your code starts here
+const helmet = require("helmet");
 
+// Use helmet middleware
+app.use(helmet());
+// Your code ends here
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Existing code provided by the course
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {

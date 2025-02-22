2d0effb5ec7abae05e93ccc4ed59fd0662555327
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-02-22 17:09:27 +0530

    feat(security): enhance security by adding frameguard and XSS protection

--- myApp.js
+++ myApp.js
@@ -8,6 +8,10 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');

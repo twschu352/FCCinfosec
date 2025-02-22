be35f1f65e1c98ddf6209c32b21fa2e8e4ed8905
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-02-22 17:24:55 +0530

    feat(security): refactor helmet configuration for improved security policies

--- myApp.js
+++ myApp.js
@@ -7,11 +7,28 @@ module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({maxAge: 90*24*60*60, force: true}));
+// app.use(helmet.dnsPrefetchControl());
+// instead of above we can use it as this
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", "'trusted-cdn.com'"]}}));
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');

4e0eee470d600817f55acf912cd992f0e2da4a83
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2025-05-04 14:21:39 +0500

    Install and require Helmet 3.21.3 for Express app security headers

--- myApp.js
+++ myApp.js
@@ -1,59 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
